a46dfe126cbff6c0b2df2b2e25abfcce7071cb1e
Svitlana <wsv3ta@gmail.com>
2024-04-28 15:58:53 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import mongoose from "mongoose";
+import { config } from 'dotenv';
+config();
+import contactsRouter from "./routes/contactsRouter.js";
+
+const app = express();
+
+mongoose.connect(process.env.DB_HOST).then(() => console.log("Database connect successfully")).catch(error => {
+  console.log(error.message);
+  process.exit(1); 
+});
+
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
+
+app.use((_, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running. Use our API on port: 3000");
+});
---
+++ schemas/contactsSchemas.js
@@ -0,0 +1,17 @@
+import Joi from "joi";
+
+export const createContactSchema = Joi.object({
+    name: Joi.string().min(3).max(30).required(),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'uk'] } })
+        .required(),
+    phone: Joi.string().min(7)
+        .max(16)
+        .required(),
+})
+
+export const updateContactSchema = Joi.object({
+    name: Joi.string().min(3).max(30),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    phone: Joi.string().min(7)
+        .max(16),
+});
